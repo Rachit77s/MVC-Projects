7ad436ddf0d7d5245a2c7a0c32522b379999232e
Rachit Srivastava <rachitsrivastava77@gmail.com>
2019-04-07 00:12:16 +0530

    SearchFunctionality in MVC5.0

---
+++ SearchFunctionality/SearchFunctionality/Controllers/HomeController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SearchFunctionality.Models;
+
+namespace SearchFunctionality.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SampleDbContext db = new SampleDbContext();
+
+        // GET: Home
+        public ActionResult Index(string searchBy, string search)
+        {
+            if (searchBy == "Gender")
+            {
+                return View(db.tblEmployees.Where(x => x.Gender == search || search == null).ToList());
+            }
+            else
+            {
+                return View(db.tblEmployees.Where(x => x.Name.StartsWith(search) || search == null).ToList());
+            }
+            
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblEmployee tblEmployee = db.tblEmployees.Find(id);
+            if (tblEmployee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblEmployee);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Gender,Email")] tblEmployee tblEmployee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblEmployees.Add(tblEmployee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblEmployee);
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblEmployee tblEmployee = db.tblEmployees.Find(id);
+            if (tblEmployee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblEmployee);
+        }
+
+        // POST: Home/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Gender,Email")] tblEmployee tblEmployee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblEmployee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblEmployee);
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblEmployee tblEmployee = db.tblEmployees.Find(id);
+            if (tblEmployee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblEmployee);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblEmployee tblEmployee = db.tblEmployees.Find(id);
+            db.tblEmployees.Remove(tblEmployee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
