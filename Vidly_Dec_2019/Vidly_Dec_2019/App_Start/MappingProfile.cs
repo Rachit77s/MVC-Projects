a844be8c2189324e728ebd2e6e58051f2516f941
Rachit77s <32384410+Rachit77s@users.noreply.github.com>
2020-01-23 22:52:53 +0530

    Added Customer DTO

---
+++ Vidly_Dec_2019/Vidly_Dec_2019/App_Start/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly_Dec_2019.DTOS;
+using Vidly_Dec_2019.Models;
+
+namespace Vidly_Dec_2019.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Customer, CustomerDto>();
+            });
+
+            IMapper mapper = config.CreateMapper();
+           // var source = new Source();
+            //var dest = mapper.Map<Customer, CustomerDto>(source);
+        }
+    }
+}
---
+++ Vidly_Dec_2019/Vidly_Dec_2019/DTOS/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly_Dec_2019.Models;
+
+namespace Vidly_Dec_2019.DTOS
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+            
+        [Min18YearsIfAMember]
+        public Nullable<System.DateTime> BirthDate { get; set; }
+
+
+        public int MembershipTypeId { get; set; }
+       // public virtual MembershipType MembershipType { get; set; }
+    }
+}
