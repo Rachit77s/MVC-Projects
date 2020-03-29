35735b50d1bbcb4a67e5db9ee25eb7054f2688da
Rachit77s <32384410+Rachit77s@users.noreply.github.com>
2020-03-29 14:51:30 +0530

    Crud Operation with Datatable

---
+++ DatatableCrud/DatatableCrud/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using DatatableCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatatableCrud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (TestRachitEntities db = new TestRachitEntities())
+            {
+                List<Employee> empList = db.Employees.ToList();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+            
+        }
+
+        [HttpGet]
+        public ActionResult AddorEdit(int id=0)
+        {
+
+            return View(new Employee());
+        }
+
+        [HttpPost]
+        public ActionResult AddorEdit(Employee employee)
+        {
+            using (TestRachitEntities db = new TestRachitEntities())
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return Json(new {success = true, message = "Saved successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
