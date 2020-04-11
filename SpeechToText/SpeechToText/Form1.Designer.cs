4057cdee93911ed894e359f2c875bc3a66085dfb
Rachit77s <32384410+Rachit77s@users.noreply.github.com>
2020-04-11 15:43:57 +0530

    Speech To Text Program

---
+++ SpeechToText/SpeechToText/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SpeechToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 157);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(154, 213);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 40);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(337, 213);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(88, 40);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 322);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ SpeechToText/SpeechToText/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+
+namespace SpeechToText
+{
+    public partial class Form1 : Form
+    {
+
+        SpeechSynthesizer synthesizerObject = new SpeechSynthesizer();
+        PromptBuilder promptObject = new PromptBuilder();
+        SpeechRecognitionEngine speechRecognitionObject = new SpeechRecognitionEngine();
+        Choices choicesListObject = new Choices();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            textBox1.Text = String.Empty;
+
+            //This is custom Grammar
+            choicesListObject.Add(new string[] { "Hello", "How are you", "Current location", "Current time", "Open Chrome",  "Welcome", "Thank you", "Close" });
+            Grammar grammar = new Grammar(new GrammarBuilder(choicesListObject));
+
+            //This is system generated Grammar
+            //Grammar grammar = new DictationGrammar();
+
+            try
+            {
+                speechRecognitionObject.RequestRecognizerUpdate();
+                speechRecognitionObject.LoadGrammar(grammar);
+                //Auto generated
+                speechRecognitionObject.SpeechRecognized += SpeechRecognitionObject_SpeechRecognized; //This is event handler auto generated
+                speechRecognitionObject.SetInputToDefaultAudioDevice();
+                speechRecognitionObject.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, " Error");
+            }
+
+
+        }
+
+        ////This is the event handler
+        private void SpeechRecognitionObject_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //MI
+            //textBox1.Text = textBox1.Text + e.Result.Text.ToString() + Environment.NewLine;
+
+            //MII
+            switch (e.Result.Text.ToString())
+            {
+                case "Hello":
+                    synthesizerObject.SpeakAsync("Hello Rachit");
+                    break;
+                case "How are you":
+                    synthesizerObject.SpeakAsync("I am doing great Rachit how about you");
+                    break;
+                case "Current location":
+                    synthesizerObject.SpeakAsync("current location is Jaipur");
+                    break;
+                case "Current time":
+                    synthesizerObject.SpeakAsync("Current time is " +
+                                                 DateTime.Now.ToLongTimeString());
+                    break;
+                case "Thank you":
+                    synthesizerObject.SpeakAsync("Pleasure is all mine Rachit");
+                    break;
+                case "Open Chrome":
+                    Process.Start("chrome", "https://www.google.com/");
+                    break;
+                case "Close":
+                    Application.Exit();
+                    break;
+            }
+            textBox1.Text = textBox1.Text + e.Result.Text.ToString() + Environment.NewLine;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            speechRecognitionObject.RecognizeAsyncStop();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+    }
+}
